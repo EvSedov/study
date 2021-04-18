0d4cc00d0836c120037d00f0f5d4c0bf437c676b
Evgeniy Sedov <kokchix@gmail.com>
2021-04-19 00:21:27 +0700

    task fp2 completed

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,48 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each(&func)
+        for item in self
+          func.call(item)
+        end
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map(&func)
+        new_array = []
+        for item in self
+          new_array << func.call(item)
+        end
+        MyArray.new(new_array)
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact(&func)
+        new_array = []
+        for item in self
+          predicate = func.nil? ? item.nil? : func.call(item)
+          new_array << item unless predicate
+        end
+        MyArray.new(new_array)
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(memo = nil, operator = nil, &block)
+        block = if operator == Symbol
+                  ->(acc, value) { acc.send(operator, value) }
+                elsif operator.nil?
+                  block
+                end
+        new_array = self
+        if memo.nil?
+          new_array = self[1..-1]
+          memo = self[0]
+        end
+        for item in new_array
+          memo = block.call(memo, item)
+        end
+        memo
+      end
     end
   end
 end
